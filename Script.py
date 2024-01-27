05e91ca350bdf43736a29074eaa6cf55b77ac3e4
Blgt11147 <131634112+Blgt11147@users.noreply.github.com>
2024-01-27 17:06:45 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -232,7 +232,7 @@ Tɪᴘ: Sᴇʟᴇᴄᴛ "Lᴀɴɢᴜᴀɢᴇs" ᴏʀ "Sᴇᴀsᴏɴs" Bᴜᴛᴛ
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {file_name}
+    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {file_name}
 ╔════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╗
 ▫️<a href=https://t.me/baljeetsingh_11147> ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ </a>
 ▫️<a href=https://t.me/baljeetsingh_11147> ʀᴇǫᴜᴇsᴛ ᴍᴏᴠɪᴇs​ </a>
